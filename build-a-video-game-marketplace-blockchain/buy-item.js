0bd3451ccf95a406777af7323aa272099012ec8d
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-10-19 16:30:11 -0300

    Create buy-item.js

---
+++ build-a-video-game-marketplace-blockchain/buy-item.js
@@ -0,0 +1,31 @@
+import { getTransactions, writeTransactions, getItemPrice, getAddressBalance } from './blockchain-helpers.js';
+
+import EC from 'elliptic';
+const ec = new.EC.ec('p192');
+
+const buyerPrivateKey = process.argv[2];
+const itemBought = process.argv[3];
+
+const keyPair = ec.genKeyPair();
+const publicKey = keyPair.getPublic('hex');
+const privateKey = keyPair.getPrivate('hex');
+
+const price = getItemPrice(itemBought);
+
+const transactions = getTransactions();
+
+const signature = keyPair.sign(publicKey + price + itemBought).toDER('hex');
+
+const newTransaction = {
+  buyerAddress: publicKey,
+  sellerAddress: null,
+  price: price,
+  itemBought: itemBought,
+  signature: signature
+}
+
+const balance = getAddressBalance(publicKey);
+if(balance >= price){
+  transactions.push(newTransaction);
+  writeTransactions(transactions);
+}
